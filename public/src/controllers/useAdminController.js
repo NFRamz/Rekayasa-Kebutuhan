94555808c3b210be5e57767bda8e6f1594a38bb6
NFRamz <133985045+NFRamz@users.noreply.github.com>
2026-03-09 23:34:15 +0700

    Make verifyAlumni function async
    Change verifyAlumni function to be asynchronous.

--- public/src/controllers/useAdminController.js
+++ public/src/controllers/useAdminController.js
@@ -1,7 +1,7 @@
 export const useAdminController = (alumniDB, setAlumniDB) => {
   const pendingData = alumniDB.filter(a => a.status === 'Menunggu Verifikasi');
 
-  const verifyAlumni = (id, isValid) => {
+  const verifyAlumni = async (id, isValid) => {
     if(isValid) {
       setAlumniDB(alumniDB.map(a => a.id === id ? { ...a, status: 'Terverifikasi' } : a));
     } else {
