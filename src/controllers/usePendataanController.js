dc4ca785992f1990c1865c6ea864c07ed9c1e166
NFRamz <133985045+NFRamz@users.noreply.github.com>
2026-03-12 18:15:50 +0700

    Update usePendataanController.js

--- src/controllers/usePendataanController.js
+++ src/controllers/usePendataanController.js
@@ -7,12 +7,47 @@ const USE_SUPABASE = Boolean(SUPABASE_URL && SUPABASE_KEY);
 export const usePendataanController = (alumniDB, setAlumniDB) => {
   const [formData, setFormData] = useState({ nama: '', nim: '', prodi: '', kampus: '', tahun: '', pekerjaan: '', instansi: '', alamat: '' });
   const [success, setSuccess] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const submitData = async (e) => {
     e.preventDefault();
-    const newAlumni = { ...formData, id: Date.now(), status: 'Menunggu Verifikasi' };
+    setIsSubmitting(true);
+    
+    let generatedLat = null;
+    let generatedLng = null;
+
+    // Mngubah alamat menjadi Lat & Lng via  API
+    try {
+
+      const query = encodeURIComponent(formData.alamat);
+      const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${query}`);
+      const data = await response.json();
+      
+
+      if (data && data.length > 0) {
+        generatedLat = parseFloat(data[0].lat);
+        generatedLng = parseFloat(data[0].lon);
+      }
+    } catch (error) {
+      console.error("Gagal melakukan geocoding:", error);
+    }
+
+    if (!generatedLat || !generatedLng) {
+      generatedLat = -7.25; 
+      generatedLng = 112.75;
+    }
+
+
+    const newAlumni = { 
+      ...formData, 
+      id: Date.now(), 
+      status: 'Menunggu Verifikasi',
+      lat: generatedLat,
+      lng: generatedLng
+    };
     
     setAlumniDB([...alumniDB, newAlumni]);
+
 
     if (USE_SUPABASE) {
       try {
@@ -24,10 +59,11 @@ export const usePendataanController = (alumniDB, setAlumniDB) => {
       } catch (err) { console.error("Gagal simpan ke DB", err); }
     }
     
+    setIsSubmitting(false);
     setSuccess(true);
     setFormData({ nama: '', nim: '', prodi: '', kampus: '', tahun: '', pekerjaan: '', instansi: '', alamat: '' });
     setTimeout(() => setSuccess(false), 3000);
   };
 
-  return { formData, setFormData, submitData, success };
+  return { formData, setFormData, submitData, success, isSubmitting };
 };
